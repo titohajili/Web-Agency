0921a3585ba5262ae38d7b594ae05e1adc3eee6d
titohajili <maqatoto963@gmail.com>
2026-02-04 19:08:24 +0400

    Scroll To Top

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Provider from "@/components/Hoc/Provider";
 import ResponsiveNav from "@/components/Home/Navbar/ResponsiveNav";
 import Footer from "@/components/Home/Footer/Footer";
+import ScrollToTop from "@/components/Helper/ScrollToTop";
 
 const font = Rubik({
   weight:["300", "400", "500", "600", "700", "800", "900"],
@@ -29,6 +30,7 @@ export default function RootLayout({
           <ResponsiveNav/>
           {children}
           <Footer/>
+          <ScrollToTop/>
         </Provider>
       </body>
     </html>
---
+++ components/Helper/ScrollToTop.tsx
@@ -0,0 +1,36 @@
+"use client"
+import React, { useEffect, useState } from 'react'
+import { FaArrowUp } from 'react-icons/fa6'
+const ScrollToTop = () => {
+
+    const [isVisible,setIsVisible] = useState(false)
+
+    useEffect(()=>{
+        const toggleVisibility = () =>{
+            if(window.scrollY > 300) setIsVisible(true)
+                else setIsVisible(false)
+        }
+
+        window.addEventListener('scroll', toggleVisibility)
+        return ()=> window.removeEventListener('scroll', toggleVisibility)
+    },[])
+
+    const scrollToTop = () =>{
+        window.scrollTo({
+            top:0,
+            behavior:'smooth'
+        })
+    }
+
+  return (
+    <div className='fixed right-4 bottom-4 animate-pulse'>
+      {isVisible && (
+        <button className='bg-blue-700 cursor-pointer text-white rounded-full w-12 h-12 flex items-center justify-center focus:outline-none' onClick={scrollToTop}>
+            <FaArrowUp />
+        </button>
+      )}
+    </div>
+  )
+}
+
+export default ScrollToTop
