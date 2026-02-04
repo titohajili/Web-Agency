939b120a2e26dbcb3fb0ad1d67dc2c0b3a70133d
titohajili <maqatoto963@gmail.com>
2026-02-04 18:32:58 +0400

    Footer finish

--- components/Home/Footer/Footer.tsx
+++ components/Home/Footer/Footer.tsx
@@ -1,5 +1,7 @@
 import Logo from '@/components/Helper/Logo'
+import Image from 'next/image'
 import React from 'react'
+import { BiCalendar } from 'react-icons/bi'
 import { FaFacebook, FaFacebookF, FaInstagram, FaPaperPlane, FaPhone, FaYoutube } from 'react-icons/fa'
 import { IoMailOpen } from 'react-icons/io5'
 import { MdKeyboardDoubleArrowRight } from 'react-icons/md'
@@ -56,11 +58,53 @@ const Footer = () => {
             <div className='mt-8'>
                 <div className='flex items-center mb-2'>
                     <MdKeyboardDoubleArrowRight className='w-7 h-7 text-white' />
-                    <p>UI Desing</p>
+                    <p className='font-medium text-sm text-white'>UI Desing</p>
+                </div>
+
+                <div className='flex items-center mb-2'>
+                    <MdKeyboardDoubleArrowRight className='w-7 h-7 text-white' />
+                    <p className='font-medium text-sm text-white'>UX Desing</p>
+                </div>
+
+                <div className='flex items-center mb-2'>
+                    <MdKeyboardDoubleArrowRight className='w-7 h-7 text-white' />
+                    <p className='font-medium text-sm text-white'>Web App</p>
+                </div>
+
+                <div className='flex items-center mb-2'>
+                    <MdKeyboardDoubleArrowRight className='w-7 h-7 text-white' />
+                    <p className='font-medium text-sm text-white'>Digital Marketing</p>
+                </div>
+
+                <div className='flex items-center mb-2'>
+                    <MdKeyboardDoubleArrowRight className='w-7 h-7 text-white' />
+                    <p className='font-medium text-sm text-white'>Video Editing</p>
                 </div>
             </div>
 
         </div>
+        {/* 4th part */}
+        <div className='mx-auto'>
+            <h1 className='text-lg font-bold text-white'>Newsletter</h1>
+            <div className='mt-8'>
+                <p className='font-medium text-sm text-white'>It is a long established fact that a reader will be distracted</p>
+                <div className='mt-5 flex items-center space-x-4'>
+                    <Image src={"/images/f.jpg"} alt='img' width={80} height={25} />
+                    <div className='flex-1'>
+                        <div className='flex items-center'>
+                            <BiCalendar className='text-amber-300 w-6 h-6 mr-2'/>
+                            <p className='text-white text-sm font-semibold'>25 October,2025</p>
+                        </div>
+
+                        <h1 className='mt-2 text-white font-bold'>The standard chunk of Lorem, ipsum.</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+      </div>
+      {/* last part */}
+      <div className='border-t-2 w-[80%] mx-auto border-gray-700 mt-8'>
+        <p className='mt-8 text-gray-300 text-center'>Copyright © 2023 All Rights Reserved</p>
       </div>
     </div>
   )
